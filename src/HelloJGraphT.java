7fa59cacd5cc19662571d304c600474458721edc
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2020-10-19 19:59:55 +0200

    Test algorithme de Prim

--- src/HelloJGraphT.java
+++ src/HelloJGraphT.java
@@ -1,21 +1,3 @@
-/*
- * (C) Copyright 2003-2020, by Barak Naveh and Contributors.
- *
- * JGraphT : a free Java graph-theory library
- *
- * See the CONTRIBUTORS.md file distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the
- * GNU Lesser General Public License v2.1 or later
- * which is available at
- * http://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR LGPL-2.1-or-later
- */
-
 import org.jgrapht.*;
 import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm;
 import org.jgrapht.graph.*;
@@ -29,16 +11,9 @@ import java.net.*;
 import java.util.*;
 
 
-/**
- * A simple introduction to using JGraphT.
- *
- * @author Barak Naveh
- */
 public final class HelloJGraphT
 {
-    private HelloJGraphT()
-    {
-    } // ensure non-instantiability.
+    private HelloJGraphT() { } // ensure non-instantiability.
 
 
     public static void main(String[] args) throws URISyntaxException, ExportException
@@ -55,35 +30,9 @@ public final class HelloJGraphT
 
     }
 
-
-    private static Graph<String, DefaultEdge> createStringGraph()
-    {
-        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
-
-        String v1 = "v1";
-        String v2 = "v2";
-        String v3 = "v3";
-        String v4 = "v4";
-
-        // add the vertices
-        g.addVertex(v1);
-        g.addVertex(v2);
-        g.addVertex(v3);
-        g.addVertex(v4);
-
-        // add edges to create a circuit
-        g.addEdge(v1, v2);
-        g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v1);
-
-        return g;
-    }
-
     private static Graph<String, DefaultWeightedEdge> createWeightedStringGraph()
     {
         Graph<String, DefaultWeightedEdge> g = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
-
 
         g.addVertex("a");
         g.addVertex("b");
@@ -101,7 +50,6 @@ public final class HelloJGraphT
         g.setEdgeWeight("e", "d", 2.d);
         g.setEdgeWeight("e", "c", 2.d);
         g.setEdgeWeight("e", "a", 8.d);
-
 
         return g;
     }
